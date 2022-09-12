fbc19d510234020900b826632b95d721f915736d
alist <alist@DESKTOP-B4L4V1A>
2022-09-12 20:40:13 +0700

    Disabled depricated, warning, notice and strict messages

--- public/index.php
+++ public/index.php
@@ -1,14 +1,10 @@
 <?php
 
-error_reporting(E_ALL);
-//error_reporting(E_ALL & ~(E_NOTICE | E_WARNING | E_DEPRECATED | E_STRICT));
-//
+error_reporting(E_ALL & ~(E_NOTICE | E_WARNING | E_DEPRECATED | E_STRICT));
+
 // Define path to application directory
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(__DIR__ . '/../application'));
 
-// Define application environment
-//defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
-//
 // load composer autoload
 require_once realpath(__DIR__ . '/../vendor/autoload.php');
 
